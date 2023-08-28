c91b84f334702f1b7efb30473d83db9bc1a55a5b
charmainemimie <82668352+charmainemimie@users.noreply.github.com>
2023-08-28 21:30:31 +0100

    installed tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+/* export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} */
+
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
