48da696e2f6ce6f2c2afadb9fca82bf2e7a35263
Charmaine Simangaliso Mangorima <82668352+charmainemimie@users.noreply.github.com>
2024-06-28 12:44:21 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     
 <section className="bg-center bg-no-repeat bg-hero_pattern bg-gray-700 bg-blend-multiply bg-cover mt-0">
     <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-44">
-        <p className=" text-white text-6xl mb-6 font-cursive">Welcome to <span className="font-extrabold font-cursive">Kure Logistics</span></p>
+        <p className=" text-white text-6xl mb-6 font-cursive"><span className="font-extrabold font-cursive">Kure Logistics</span></p>
         {/* <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-3xl lg:text-4xl font-sans"></h1> */}
         <p className="mb-8 text-lg font-normal text-white lg:text-xl sm:px-16 lg:px-48">Efficiency in Motion, Logistics in Action. </p>
         
